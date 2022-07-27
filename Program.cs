837321154df53b82921916d18885a1134f75407a
irina <irina@local.ru>
2022-07-27 12:06:44 +0300

    ДЗ09

--- Program.cs
+++ Program.cs
@@ -113,6 +113,93 @@ void Задача56()
  Console.WriteLine();
  Console.WriteLine($"Минимальная сумма находится в строке {sumMinIndex + 1} и равна {sumMin}");
 }
-Задача56();
+///Задача56();
 
 ///Задача54();
+void Задача64()
+ {
+   /*Задача 64: Задайте значения M и N. Напишите программу, которая выведет 
+   все натуральные числа в промежутке от M до N, кратные трём.
+   M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+   M = 4; N = 8. -> ""4, 6, 7, 8""*/
+ Console.WriteLine("Введите число M");
+ int count = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите число N");
+ int number = Convert.ToInt32(Console.ReadLine());
+ Recursion(count, number);
+ Console.WriteLine();
+
+ void Recursion(int count, int number)
+ {
+   if(count > number) return;
+   Console.Write(count + " ");
+   count++;
+   Recursion(count, number);
+ }
+  for (int i = 1; i <= number; i++)
+ {
+   if (i%3 == 0)
+   {
+    Console.Write(i + " ");
+   }  
+ }
+ Console.WriteLine();
+
+ }
+void Задача66()
+ {
+  /* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+  натуральных элементов в промежутке от M до N.
+  M = 1; N = 15 -> 120
+  M = 4; N = 8. -> 30 */
+ Console.WriteLine("Введите число M");
+ int count = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите число N");
+ int number = Convert.ToInt32(Console.ReadLine());
+ Recursion(count, number);
+ Console.WriteLine();
+ 
+ void Recursion(int count, int number)
+ {
+   if(count > number) return;
+   Console.Write(count + " ");
+   count++;
+   Recursion(count, number);
+ }
+  int sum = 0;
+    for (int i = count; i <= number; i++)
+  {
+   sum = sum + i;
+  }
+  
+     Console.WriteLine("сумма чисел равна: " + sum);
+    
+ }
+
+void Задача68()
+ {
+   
+  /*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+  Даны два неотрицательных числа m и n.
+  m = 3, n = 2 -> A(m,n) = 29 */
+
+    Console.WriteLine("Введите число M");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число N");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Recursion(m, n);
+
+ int Recursion(int m, int n)
+ {
+    if (m == 0)
+        return n + 1;
+    if (n == 0)
+        return Recursion(m - 1, 1);
+    return Recursion(m - 1, Recursion(m, n - 1));
+    Recursion(m, n);
+ }
+    Console.WriteLine($"A(m,n) = {Recursion(m, n)}");
+ }
+ //Задача68();
+ ///Задача66();
+///Задача64();
